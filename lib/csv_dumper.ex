530ca702e28d9f7ab336f1626f6379cef21536d1
Sigit <dewa19@gmail.com>
2019-07-08 14:32:40 -0500

    initial blank commit

---
+++ lib/csv_dumper.ex
@@ -0,0 +1,18 @@
+defmodule CsvDumper do
+  @moduledoc """
+  Documentation for CsvDumper.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CsvDumper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/csv_dumper_test.exs
@@ -0,0 +1,8 @@
+defmodule CsvDumperTest do
+  use ExUnit.Case
+  doctest CsvDumper
+
+  test "greets the world" do
+    assert CsvDumper.hello() == :world
+  end
+end
